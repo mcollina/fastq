f23528d5e55db4338d3fc687cdcd2a518b147a89
Keri Henare <keri@henare.co.nz>
2020-05-12 17:54:02 +1200

    Adds Result type and TypeScript examples

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
-declare function fastq<C, T = any>(context: C, worker: fastq.worker<C, T>, concurrency: number): fastq.queue<T>
-declare function fastq<C, T = any>(worker: fastq.worker<C, T>, concurrency: number): fastq.queue<T>
+declare function fastq<C, T = any, R = any>(context: C, worker: fastq.worker<C, T, R>, concurrency: number): fastq.queue<T, R>
+declare function fastq<C, T = any, R = any>(worker: fastq.worker<C, T, R>, concurrency: number): fastq.queue<T, R>
 
 declare namespace fastq {
-  type worker<C, T = any> = (this: C, arg: T, cb: () => void) => void
-  type done = (err: Error, result: any) => void
+  type worker<C, T = any, R = any> = (this: C, task: T, cb: fastq.done<R>) => void
+  type done<R = any> = (err: Error | null, result?: R) => void
 
-  interface queue<T = any> {
-    push(task: T, done: done): void
-    unshift(task: T, done: done): void
+  interface queue<T = any, R = any> {
+    push(task: T, done: done<R>): void
+    unshift(task: T, done: done<R>): void
     pause(): any
     resume(): any
     idle(): boolean
